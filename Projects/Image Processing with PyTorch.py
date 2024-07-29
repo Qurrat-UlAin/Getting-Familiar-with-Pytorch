ee7bd0e4880c284197006dd1118ebca05d1d7512
Qurrat Ul Ain <120576730+Qurrat-UlAin@users.noreply.github.com>
2024-07-29 13:16:53 +0500

    Update Image Processing with PyTorch.py

--- Projects/Image Processing with PyTorch.py
+++ Projects/Image Processing with PyTorch.py
@@ -81,7 +81,131 @@ Estimated Total Size (MB): 4.11
 #2,570: Parameters in the second Linear (fully connected) layer.
 #3200: Number of features after flattening, determined by the dimensions of the output from the last convolutional layer (after pooling and flattening).
 
+#lets have a look at an image
+preds=[]
+ix=24301
+for px in range(-5,6):
+    img=tr_images[ix]/225
+    img=img.view(28,28)
+    img2=np.roll(img, px, axis=1)
+    plt.imshow(img2)
+    plt.show()
+    img3=torch.Tensor(img2).view(-1,1,28,28)
+    np_output=model(img3).cpu().detach().numpy()
+    preds.append(np.exp(np_output)/np.sum(np.exp(np_output)))
 
+#displaying porobabilty using a heatmap
+import seaborn as sns 
+fig,ax=plt.subplots(1,1,figsize=(12,10))
+plt.title("Probabaility of each class for various translations")
+sns.heatmap(np.array(preds).reshape(11,10), annot=True, 
+            ax=ax, fmt='2f', xticklabels=mnist.classes, 
+            yticklabels=[str(i)+str('pixels') for i in range(-5,6)],
+            cmap='gray'
+            )
+
+
+#Now we should augment the image for a) learning pytorch b)we have some predictions from the heatmap
+#and although they are correct, this does not solve the problem completley 
+
+#That is why image augmentation comes in place.
+#  We can create more images from a given image. 
+# Each of the created images can vary in terms of rotation, translation, scale , 
+# noise and brightness. Furthermore, the extent of the variation in each of these 
+# parameters can also vary (for example, the translation of a certain image in a given iteration can be +10 pixels while in different iteration, it can be -5 pixels)
+
+plt.imshow(tr_images[2])
+from imgagug import augmenters as iaa
+aug=iaa.Affine(scale=2)
+
+plt.imshow(aug.augment_image(tr_images[2]))
+plt.title("scaled image")
+
+aug = iaa.Affine(translate_px=10)
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Translated image by 10 pixels')
+
+aug = iaa.Affine(translate_px={'x':10, 'y':2})
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Translation of 10 pixels \naccross columns \nand 2 pixels over rows')
+
+# We will see te output when we scale, translate, rotate and shear the image
+plt.figure(figsize=(20,20))
+plt.subplot(161)
+plt.imshow(tr_images[0])
+plt.title('Original image')
+plt.subplot(162)
+aug = iaa.Affine(scale=2, fit_output=True)
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Scaled image')
+plt.subplot(163)
+aug = iaa.Affine(translate_px={'x':10, 'y':2}, fit_output=True)
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Translation of 10 pixels accross \ncolumns and \n2 pixels over rows')
+plt.subplot(164)
+aug = iaa.Affine(rotate=60, fit_output=True)
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Rotation of image \nby 60 degrees')
+plt.subplot(165)
+aug = iaa.Affine(shear=25, fit_output=True)
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Shear the image \nby 25 degrees')
+
+aug = iaa.Affine(rotate=60, fit_output=True,cval=255)
+plt.imshow(aug.augment_image(tr_images[0]))
+plt.title('Rotation of image \nby 60 degrees')
+
+plt.figure(figsize=(20,20))
+plt.subplot(141)
+aug = iaa.Affine(rotate=(-45,45), fit_output=True, cval=0, \
+                 mode='constant')
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray')
+plt.subplot(142)
+aug = iaa.Affine(rotate=(-45,45), fit_output=True, cval=0, \
+                 mode='constant')
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray')
+plt.subplot(143)
+aug = iaa.Affine(rotate=(-45,45), fit_output=True, cval=0, \
+                 mode='constant')
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray')
+plt.subplot(144)
+aug = iaa.Affine(rotate=(-45,45), fit_output=True, cval=0, \
+                 mode='constant')
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray')
+
+aug = iaa.Multiply(0.5)
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray', \
+           vmin= 0, vmax=255)
+plt.title('Pixels multiplied by 0.5')
+
+aug = iaa.LinearContrast(0.5)
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray', \
+           vmin= 0, vmax=255)
+plt.title('Pixels multiplied by 0.5')
+
+# Adding noise
+# There 2 main methods that help us to simulate the grainy image. They are Dropout and SaltAndPepper
+# In real world we will may face the bad image due to bad photography
+plt.figure(figsize=(10,10))
+plt.subplot(121)
+aug = iaa.Dropout(p=0.2)
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray', \
+           vmin= 0, vmax=255)
+plt.title('Random 30% of dropout')
+plt.subplot(122)
+aug = iaa.SaltAndPepper(0.2)
+plt.imshow(aug.augment_image(tr_images[0]), cmap='gray', \
+           vmin= 0, vmax=255)
+plt.title('Random 30% salt and peper noise')
+
+sequential = iaa.Sequential(
+    [
+     iaa.Dropout(p=0.2), iaa.Affine(rotate=(-30,30))
+    ], random_order=True
+)
+plt.imshow(sequential.augment_image(tr_images[0]), cmap='gray', \
+           vmin=0, vmax=255)
+plt.title('Image augmented using a \nrandom order of the 2 augmentations')
 
 
 
